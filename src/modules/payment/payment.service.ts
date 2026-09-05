7af95b601b4b49d41432f03bcae8ae37cc675f3d
Mohosin <mohosinali075@gmail.com>
2026-09-05 10:53:08 +0600

    feat: implement Stripe checkout and payment verification service and update project status logs

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -60,7 +60,7 @@ const createCheckoutSession = async (
       ],
       mode: 'payment',
       success_url: `${config.clientUrl}?session_id={CHECKOUT_SESSION_ID}&success=true`,
-      cancel_url: `${config.clientUrl}/cancel?success=false`,
+      cancel_url: `${config.clientUrl}/payment-failed?success=false`,
       customer_email: user.email,
       metadata: {
         userId: user.authId.toString(),
